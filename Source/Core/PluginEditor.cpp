1602e8ce923c06cb07f2237e6d9cd156dec6b109
chrisdevmeow <devvingisntaproblem@gmail.com>
2026-06-26 15:04:18 -0600

    Update PluginEditor.cpp

--- Source/Core/PluginEditor.cpp
+++ Source/Core/PluginEditor.cpp
@@ -1,152 +1,154 @@
 #include "PluginEditor.h"
 
 ZenithAudioEditor::ZenithAudioEditor (ZenithAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
+    : AudioProcessorEditor (&p), audioProcessor (p), effectPanel (p.getEffectRack())
 {
-    // ========== SYNTH SECTION ==========
     addAndMakeVisible (waveSelector);
     waveSelector.addItemList ({ "Sine", "Saw", "Square", "Triangle" }, 1);
     waveAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ComboBoxAttachment> (
         audioProcessor.apvts, "waveform", waveSelector);
 
-    addSlider (freqSlider, freqAttachment, "freq", "Freq");
-    addSlider (fineSlider, fineAttachment, "fine", "Fine");
-    addSlider (cutoffSlider, cutoffAttachment, "cutoff", "Cutoff");
-    addSlider (resonanceSlider, resonanceAttachment, "resonance", "Res");
-    addSlider (filterEnvSlider, filterEnvAttachment, "filterEnv", "F.Env");
+    addKnob (freqKnob, freqAttachment, "freq", "Freq");
+    addKnob (fineKnob, fineAttachment, "fine", "Fine");
+    addKnob (cutoffKnob, cutoffAttachment, "cutoff", "Cutoff");
+    addKnob (resonanceKnob, resonanceAttachment, "resonance", "Res");
+    addKnob (filterEnvKnob, filterEnvAttachment, "filterEnv", "F.Env");
+    addKnob (attackKnob, attackAttachment, "attack", "A");
+    addKnob (decayKnob, decayAttachment, "decay", "D");
+    addKnob (sustainKnob, sustainAttachment, "sustain", "S");
+    addKnob (releaseKnob, releaseAttachment, "release", "R");
+    addKnob (lfoRateKnob, lfoRateAttachment, "lfoRate", "Rate");
+    addKnob (lfoDepthKnob, lfoDepthAttachment, "lfoDepth", "Depth");
 
-    addSlider (attackSlider, attackAttachment, "attack", "A");
-    addSlider (decaySlider, decayAttachment, "decay", "D");
-    addSlider (sustainSlider, sustainAttachment, "sustain", "S");
-    addSlider (releaseSlider, releaseAttachment, "release", "R");
+    addAndMakeVisible (lfoTargetSelector);
+    lfoTargetSelector.addItemList ({ "Pitch", "Filter", "Amp" }, 1);
+    lfoTargetAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ComboBoxAttachment> (
+        audioProcessor.apvts, "lfoTarget", lfoTargetSelector);
 
-    // ========== MODULATION ==========
-    addSlider (lfoRateSlider, lfoRateAttachment, "lfoRate", "Rate");
-    addSlider (lfoDepthSlider, lfoDepthAttachment, "lfoDepth", "Depth");
-    addComboBox (lfoTargetSelector, lfoTargetAttachment, "lfoTarget", { "Pitch", "Filter", "Amp" });
+    addKnob (chorusKnob, chorusAttachment, "chorusMix", "Chorus");
+    addKnob (delayKnob, delayAttachment, "delayMix", "Delay");
+    addKnob (reverbKnob, reverbAttachment, "reverbMix", "Reverb");
+    addKnob (masterKnob, masterAttachment, "masterGain", "Master");
 
-    // ========== EFFECTS ==========
-    addSlider (chorusSlider, chorusAttachment, "chorusMix", "Chorus");
-    addSlider (delaySlider, delayAttachment, "delayMix", "Delay");
-    addSlider (reverbSlider, reverbAttachment, "reverbMix", "Reverb");
-
-    // ========== MASTER ==========
-    addSlider (masterSlider, masterAttachment, "masterGain", "Master");
-
-    // ========== CHORD ==========
-    addComboBox (chordRootSelector, chordRootAttachment, "chordRoot", { "C","C#","D","D#","E","F","F#","G","G#","A","A#","B" });
-    addComboBox (chordTypeSelector, chordTypeAttachment, "chordType", { "Major","Minor","7","Maj7","Sus2","Sus4" });
-
-    chordPlayButton.onClick = [this] { audioProcessor.playChord(); };
+    addAndMakeVisible (chordRootSelector);
+    chordRootSelector.addItemList ({ "C","C#","D","D#","E","F","F#","G","G#","A","A#","B" }, 1);
+    addAndMakeVisible (chordTypeSelector);
+    chordTypeSelector.addItemList ({ "Major","Minor","7","Maj7","Sus2","Sus4" }, 1);
     addAndMakeVisible (chordPlayButton);
+    chordPlayButton.onClick = [this] { handleChordPlay(); };
 
-    // ========== PRESETS ==========
-    presetSaveButton.onClick = [this] { audioProcessor.savePreset(); };
-    presetLoadButton.onClick = [this] { audioProcessor.loadPreset(); };
     addAndMakeVisible (presetSaveButton);
     addAndMakeVisible (presetLoadButton);
+    presetSaveButton.onClick = [this] { handleSavePreset(); };
+    presetLoadButton.onClick = [this] { handleLoadPreset(); };
 
-    // ========== LOOK & FEEL ==========
+    addAndMakeVisible (effectPanel);
+
     setLookAndFeel (&lookAndFeel);
     lookAndFeel.setColour (juce::Slider::rotarySliderFillColourId, juce::Colours::cyan.darker (0.3f));
 
-    setSize (1100, 600);
-    startTimerHz (24);
+    setSize (1200, 700);
+    startTimerHz (30);
 }
 
-ZenithAudioEditor::~ZenithAudioEditor()
-{
-    setLookAndFeel (nullptr);
-}
+ZenithAudioEditor::~ZenithAudioEditor() { setLookAndFeel (nullptr); }
 
-void ZenithAudioEditor::paint (juce::Graphics& g)
-{
-    g.fillAll (juce::Colour (0xff1a1a2e)); // deep dark purple
-}
+void ZenithAudioEditor::paint (juce::Graphics& g) { g.fillAll (juce::Colour (0xff1a1a2e)); }
 
 void ZenithAudioEditor::resized()
 {
     auto area = getLocalBounds().reduced (16);
+    auto topSection = area.removeFromTop (area.getHeight() * 0.65);
+    auto synthArea = topSection.removeFromLeft (topSection.getWidth() * 0.6);
 
-    // Section widths: Synth 50%, FX + Mod 30%, Master + Presets 20%
-    auto synthArea = area.removeFromLeft (area.getWidth() * 0.5);
-    auto fxArea = area.removeFromLeft (area.getWidth() * 0.5); // remaining split
-    auto masterArea = area;
+    auto waveRow = synthArea.removeFromTop (30);
+    waveSelector.setBounds (waveRow.removeFromLeft (120));
 
-    // --- Synth Section ---
-    auto synthRow = synthArea.removeFromTop (30);
-    waveSelector.setBounds (synthRow.removeFromLeft (120));
+    auto oscRow = synthArea.removeFromTop (80);
+    freqKnob.setBounds (oscRow.removeFromLeft (80).reduced (6));
+    fineKnob.setBounds (oscRow.removeFromLeft (80).reduced (6));
 
-    auto synthGrid = synthArea;
-    auto gridCols = 5;
-    auto cellW = synthGrid.getWidth() / gridCols;
+    auto filterRow = synthArea.removeFromTop (80);
+    cutoffKnob.setBounds (filterRow.removeFromLeft (80).reduced (6));
+    resonanceKnob.setBounds (filterRow.removeFromLeft (80).reduced (6));
+    filterEnvKnob.setBounds (filterRow.removeFromLeft (80).reduced (6));
 
-    freqSlider.setBounds (synthGrid.removeFromLeft (cellW).reduced (6));
-    fineSlider.setBounds (synthGrid.removeFromLeft (cellW).reduced (6));
-    cutoffSlider.setBounds (synthGrid.removeFromLeft (cellW).reduced (6));
-    resonanceSlider.setBounds (synthGrid.removeFromLeft (cellW).reduced (6));
-    filterEnvSlider.setBounds (synthGrid.removeFromLeft (cellW).reduced (6));
+    auto adsrRow = synthArea.removeFromTop (80);
+    attackKnob.setBounds (adsrRow.removeFromLeft (80).reduced (6));
+    decayKnob.setBounds (adsrRow.removeFromLeft (80).reduced (6));
+    sustainKnob.setBounds (adsrRow.removeFromLeft (80).reduced (6));
+    releaseKnob.setBounds (adsrRow.removeFromLeft (80).reduced (6));
 
-    // ADSR row
-    auto adsrArea = synthArea.removeFromBottom (90);
-    attackSlider.setBounds (adsrArea.removeFromLeft (cellW).reduced (6));
-    decaySlider.setBounds (adsrArea.removeFromLeft (cellW).reduced (6));
-    sustainSlider.setBounds (adsrArea.removeFromLeft (cellW).reduced (6));
-    releaseSlider.setBounds (adsrArea.removeFromLeft (cellW).reduced (6));
+    auto lfoRow = synthArea.removeFromTop (80);
+    lfoRateKnob.setBounds (lfoRow.removeFromLeft (80).reduced (6));
+    lfoDepthKnob.setBounds (lfoRow.removeFromLeft (80).reduced (6));
+    lfoTargetSelector.setBounds (lfoRow.removeFromLeft (120).reduced (6));
 
-    // --- FX + Modulation ---
-    auto fxGrid = fxArea;
-    auto fxCols = 4;
-    auto fxW = fxGrid.getWidth() / fxCols;
+    auto fxArea = topSection;
+    auto fxRow = fxArea.removeFromTop (80);
+    chorusKnob.setBounds (fxRow.removeFromLeft (80).reduced (6));
+    delayKnob.setBounds (fxRow.removeFromLeft (80).reduced (6));
+    reverbKnob.setBounds (fxRow.removeFromLeft (80).reduced (6));
 
-    chorusSlider.setBounds (fxGrid.removeFromLeft (fxW).reduced (6));
-    delaySlider.setBounds (fxGrid.removeFromLeft (fxW).reduced (6));
-    reverbSlider.setBounds (fxGrid.removeFromLeft (fxW).reduced (6));
+    auto masterRow = fxArea.removeFromTop (80);
+    masterKnob.setBounds (masterRow.removeFromLeft (80).reduced (6));
 
-    // LFO row (under FX)
-    auto lfoArea = fxArea.removeFromBottom (90);
-    lfoRateSlider.setBounds (lfoArea.removeFromLeft (fxW).reduced (6));
-    lfoDepthSlider.setBounds (lfoArea.removeFromLeft (fxW).reduced (6));
-    lfoTargetSelector.setBounds (lfoArea.removeFromLeft (fxW).reduced (6));
-
-    // --- Master + Presets + Chord ---
-    auto masterRow = masterArea.removeFromTop (40);
-    masterSlider.setBounds (masterRow.removeFromLeft (120).reduced (4));
-    presetSaveButton.setBounds (masterRow.removeFromLeft (80).reduced (4));
-    presetLoadButton.setBounds (masterRow.removeFromLeft (80).reduced (4));
-
-    auto chordRow = masterArea.reduced (8);
+    auto chordRow = fxArea.removeFromTop (40);
     chordRootSelector.setBounds (chordRow.removeFromLeft (80).reduced (4));
     chordTypeSelector.setBounds (chordRow.removeFromLeft (100).reduced (4));
     chordPlayButton.setBounds (chordRow.removeFromLeft (80).reduced (4));
+
+    auto presetRow = fxArea.removeFromTop (40);
+    presetSaveButton.setBounds (presetRow.removeFromLeft (80).reduced (4));
+    presetLoadButton.setBounds (presetRow.removeFromLeft (80).reduced (4));
+
+    effectPanel.setBounds (area);
 }
 
-void ZenithAudioEditor::timerCallback()
-{
-    // update any dynamic displays if needed
-}
+void ZenithAudioEditor::timerCallback() {}
 
-// ----- helper to add sliders -----
-void ZenithAudioEditor::addSlider (juce::Slider& slider,
-                                   std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment>& attachment,
-                                   const juce::String& paramID,
-                                   const juce::String& labelText)
+void ZenithAudioEditor::addKnob (Knob& knob,
+                                 std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment>& attachment,
+                                 const juce::String& paramID,
+                                 const juce::String& labelText)
 {
-    addAndMakeVisible (slider);
-    slider.setSliderStyle (juce::Slider::RotaryVerticalDrag);
-    slider.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 50, 16);
-    slider.setColour (juce::Slider::rotarySliderFillColourId, juce::Colours::cyan.darker (0.4f));
+    addAndMakeVisible (knob);
+    knob.setTextValueSuffix (labelText);
     attachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment> (
-        audioProcessor.apvts, paramID, slider);
+        audioProcessor.apvts, paramID, knob);
 }
 
-void ZenithAudioEditor::addComboBox (juce::ComboBox& box,
-                                     std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment>& attachment,
-                                     const juce::String& paramID,
-                                     const juce::StringArray& items)
+void ZenithAudioEditor::handleChordPlay()
 {
-    addAndMakeVisible (box);
-    box.addItemList (items, 1);
-    attachment = std::make_unique<juce::AudioProcessorValueTreeState::ComboBoxAttachment> (
-        audioProcessor.apvts, paramID, box);
+    int root = chordRootSelector.getSelectedId() - 1;
+    int type = chordTypeSelector.getSelectedId() - 1;
+    audioProcessor.playChord (root, type);
+}
+
+void ZenithAudioEditor::handleSavePreset()
+{
+    juce::FileChooser chooser ("Save Preset",
+                               juce::File::getSpecialLocation (juce::File::userDocumentsDirectory)
+                                   .getChildFile ("ZenithPreset.zenith"),
+                               "*.zenith");
+    if (chooser.browseForFileToSave (true))
+    {
+        auto file = chooser.getResult();
+        juce::MemoryBlock block;
+        audioProcessor.getStateInformation (block);
+        file.replaceWithData (block.getData(), block.getSize());
+    }
+}
+
+void ZenithAudioEditor::handleLoadPreset()
+{
+    juce::FileChooser chooser ("Load Preset",
+                               juce::File::getSpecialLocation (juce::File::userDocumentsDirectory),
+                               "*.zenith");
+    if (chooser.browseForFileToOpen())
+    {
+        auto file = chooser.getResult();
+        auto data = file.loadFileAsData();
+        audioProcessor.setStateInformation (data.getData(), (int)data.getSize());
+    }
 }
