72d7360f68ff25dfb1363a1c02f7e13fe6d0f1bf
chrisdevmeow <devvingisntaproblem@gmail.com>
2026-06-26 15:11:10 -0600

    Update build.sh

--- Web/wasm/build.sh
+++ Web/wasm/build.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 em++ -O3 \
-    -I../../Source \
-    -I../../Source/DSP \
-    ../../Source/DSP/Oscillator.cpp \
-    ../../Source/DSP/Filter.cpp \
-    ../../Source/DSP/Envelope.cpp \
-    zenith_dsp.cpp \
-    -o zenith_dsp.js \
+    --bind \
     -s WASM=1 \
     -s EXPORT_NAME='ZenithModule' \
     -s MODULARIZE=1 \
     -s EXPORT_ES6=1 \
     -s ALLOW_MEMORY_GROWTH=1 \
     -s ENVIRONMENT='web' \
-    --bind
+    -s SINGLE_FILE=0 \
+    -o zenith_dsp.js \
+    zenith_dsp.cpp
